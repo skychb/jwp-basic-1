04cc3c99873be152ac8476887c7716f443782e96
Chiho <gm@haeyum.org>
2016-03-17 11:47:54 +0900

    dbbasic

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -36,12 +36,69 @@ public class UserDao {
 	}
 	
 	public void update(User user) throws SQLException {
-		// TODO 구현 필요함.
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = ConnectionManager.getConnection();
+			String userId = user.getUserId();
+			String password = user.getPassword();
+			String name = user.getName();
+			String email = user.getEmail();
+			String sql = "UPDATE USERS set password=?, name=?, email=? WHERE userid=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, password);
+			pstmt.setString(2, name);
+			pstmt.setString(3, email);
+			pstmt.setString(4, userId);
+			pstmt.executeUpdate();
+			return;
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+		}
 	}
 	
 	public List<User> findAll() throws SQLException {
-		// TODO 구현 필요함.
-		return new ArrayList<User>();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = ConnectionManager.getConnection();
+			String sql = "SELECT userId, password, name, email FROM USERS";
+			pstmt = con.prepareStatement(sql);
+			//pstmt.setString(1, userId);
+			rs = pstmt.executeQuery();
+
+			ArrayList<User> userlist = new ArrayList<User>();
+			while(rs.next()){
+				User user = new User(
+						rs.getString("userId"), 
+						rs.getString("password"), 
+						rs.getString("name"),
+						rs.getString("email"));
+				userlist.add(user);
+			}
+			//return new ArrayList<User>();//
+			return userlist;
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+		}
 	}
 
 	public User findByUserId(String userId) throws SQLException {
