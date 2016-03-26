5aa80e173647e1e7e49652c80d9b29a7c6c1bb0e
Chiho <gm@haeyum.org>
2016-03-26 17:19:25 +0900

    ajax_re

--- src/main/java/next/controller/qna/AddAnswerController.java
+++ src/main/java/next/controller/qna/AddAnswerController.java
@@ -3,6 +3,7 @@ package next.controller.qna;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,24 +11,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import core.mvc.Controller;
 import next.dao.AnswerDao;
 import next.model.Answer;
 
-@WebServlet("/qna/addanswer")
-public class AddAnswerController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+
+public class AddAnswerController implements Controller {
+
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Answer answer = new Answer(req.getParameter("writer"), req.getParameter("contents"),
+	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		Answer answer = new Answer(req.getParameter("writer"), 
+				req.getParameter("contents"), 
 				Long.parseLong(req.getParameter("questionId")));
+
 		AnswerDao answerDao = new AnswerDao();
 		Answer savedAnswer = answerDao.insert(answer);
 		ObjectMapper mapper = new ObjectMapper();
 		resp.setContentType("application/json;charset=UTF-8");
 		PrintWriter out = resp.getWriter();
 		out.print(mapper.writeValueAsString(savedAnswer));
-	}
+		return null;
+}
 }
--- webapp/qna/show.jsp
+++ webapp/qna/show.jsp
@@ -86,7 +86,7 @@
 							</article>
                             </c:forEach>
 							<div class="answerWrite">
-								<form name="answer" method="post">
+								<form name="answer" method="post" action="/api/qna/answer">
 									<input type="hidden" name="questionId" value="${question.questionId}">
 									<div class="form-group col-lg-4" style="padding-top:10px;">
 										<input class="form-control" id="writer" name="writer" placeholder="이름">
